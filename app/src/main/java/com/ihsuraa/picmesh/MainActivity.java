cadb86c700a3c220fcd5247d19e83a8e5c536a6c
lalitsahu1999 <lalitprasad9031@gmail.com>
2020-06-16 15:24:40 +0530

    Intro slider

--- app/src/main/java/com/ihsuraa/picmesh/MainActivity.java
+++ app/src/main/java/com/ihsuraa/picmesh/MainActivity.java
@@ -2,7 +2,10 @@ package com.ihsuraa.picmesh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PrefManager prefManager = new PrefManager(getApplicationContext());
+        if(prefManager.isFirstTimeLaunch()){
+            prefManager.setFirstTimeLaunch(false);
+            startActivity(new Intent(MainActivity.this, IntroActivity.class));
+            finish();
+        }
+    }
+    protected void btn_Click(View view){
+        Toast.makeText(MainActivity.this, "clicked on button", Toast.LENGTH_LONG).show();
     }
 }
